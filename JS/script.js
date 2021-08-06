40bdaed5a11bcf49cfddf60c5001f3688a66fa57
Samuel Valmir <53239510+SamuelValmir@users.noreply.github.com>
2021-08-06 19:37:36 -0300

    Making the "data base"
    It was maked the system to store just one time the local storage.
    
    Almost each element has the unique responsible principle.
    
    It was maked the system to add new tasks in html and in the local storage
    It was started to make system for delete the task.
    
    I need to make the id auto adjust itself, remake it count, or find the index of an element by searching a match of the value of each property of the element

--- JS/script.js
+++ JS/script.js
@@ -1,87 +1,144 @@
-function Task(id, task, explication, dateToFinish, dateAdded) {
+
+/*
+ localStorage.removeItem("canStorageTasksJson")
+ localStorage.clear("canStorageTasksJson")
+ localStorage.removeItem("tasksJson")
+ localStorage.clear("tasksJson")
+*/
+
+// console.log(localStorage)
+//System for not reset the value of the tasksJson stored in "local storage"
+let canStorageTasksJson = localStorage.getItem("canStorageTasksJson");
+
+if (canStorageTasksJson == null){
+    localStorage.setItem("tasksJson", "");
+    localStorage.setItem("canStorageTasksJson", false)
+    console.log("defined")
+}
+
+
+window.onload = function(){
+    let tasksJson = localStorage.getItem("tasksJson");
+    
+    //If Json tasks in local storage is not empty...
+    if (tasksJson != ""){
+        console.log(localStorage.getItem("tasksJson"));
+        tasks = JSON.parse(localStorage.getItem("tasksJson"));
+        for (const task of tasks) {
+            console.log("task:");
+            console.log(task);
+            addTask(task);
+        }
+    }
+}
+
+function deleteTask(id){
+
+    console.log("id:")
+    console.log(id)
+
+    tasks = JSON.parse(localStorage.getItem("tasksJson"));
+    tasks.splice(tasks[id - 1], 1);
+    console.log("tasks:")
+    console.log(tasks)
+    localStorage.setItem("tasksJson", tasks);
+}
+
+
+
+
+//Main Class (Task)
+function Task(id, title, explication, dateToFinish, dateAdded) {
     this.id = id;
-    this.task = task;
+    this.title = title;
     this.explication = explication;
     this.dateToFinish = dateToFinish;
     this.dateAdded = dateAdded;
     this.finishedDate;
-
-    this.getId = function () {
-        return this.id;
-    }
-
-    this.setTask = function (task) {
-        this.task = task;
-    }
-
-    this.getTask = function () {
-        return this.task;
-    }
-
-    this.setFinishedDate = function (finishedDate) {
-        this.finishedDate = finishedDate;
-    }
-
-    this.getFinishedDate = function () {
-        return this.finishedDate;
-    }
 }
 
-function checkFields(element) {
-    transformToJson();
-    saveTask();
-    return
-    let task = document.getElementsByName('taskForm')[0]
-    if (task.value == '' || task.value == null || task.value == undefined) {
-        alert('Task needs to be filled!')
-    } else {
-        // alert('Task Saved with success!')
-    }
-    //* window.location.href = 'index.html';
-}
+function addTask(task, tasks){
 
-function saveTask() {
-    let div = document.getElementById('container');
-
-    div.innerHTML +=
-    ("<div id='taskContainer'>" +
+    function findIndex(list, obj) {
+        return list.findIndex((current) =>
+          Object.keys(current).every((key) => obj[key] === current[key])
+        );
+      }
+    let container = document.getElementById('container');
+    container.innerHTML +=
+    "<div id='taskContainer'>" +
         "<div id='checkContainer'>" + 
             "<input type='checkbox' id='completed'>" + 
         "</div>" + 
         
         "<div id='descriptionContainer'>" + 
-            "<h4 id='task'>"+ "abcdefg" +"</h4>" + 
+            "<h4 id='task'>"+ task.title +"</h4>" + 
 
             "<div id='explicationContainer'>" +
-                "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa in placeat accusamus, velit facilis eveniet dolorum fuga est esse eaque rerum explicabo laudantium dicta fugit ratione fugiat nihil, quasi veritatis!" +
+                task.explication+
             "</div>" +
 
-            "<p id='dateToFinish'>06/08/2021</p>" +
+            "<p id='dateToFinish'>" + task.dateToFinish + "</p>" +
         "</div>" +
 
         "<div id='editContainer' class='icon'>" +
             "<img src='./Assets/Icons/edit-icon.png' alt='edit icon' title='edit icon'>" +
         "</div>" +
 
-        "<div id='deleteContainer' class='icon'>" +
+        "<div id='deleteContainer' class='icon' onclick='deleteTask(" + findIndex(tasks, task) + ")'>" +
+        //!I need to make the id auto adjust itself, remake it count, or find the index of an element by searching a match of the value of each property of the element
             "<img src='./Assets/Icons/delete-icon.png' alt='delete icon' title='delete icon'>" +
         "</div>" +
-    "</div>");
-
+    "</div>";
 }
 
-function transformToJson(){
-    let tasks = Array();
-    
-    let id = tasks.length + 1;
-    let task = document.getElementById('taskForm');
-    let explication = document.getElementById('explicationForm');
-    let dateToFinish = document.getElementById('dateToFinishForm');
-    let timeToFinish = document.getElementById('timeToFinishForm');
+function checkFields(element) {
+    saveTask();
+    loadNewTask();
+    return
+    let task = document.getElementsByName('taskForm')[0]
+    if (task.value == '' || task.value == null || task.value == undefined) {
+        alert('Task title must be filled!')
+    } else {
+        // alert('Task Saved with success!')
+    }
+    //* window.location.href = 'index.html';
+}
 
+function loadNewTask() {
+    //Parses json tasks to array
+    let tasks = JSON.parse(localStorage.getItem("tasksJson"));
+
+    //Picks up the last item of the task array
+    let task = tasks[tasks.length - 1];
+
+    //Adds a task on the html
+    addTask(task, tasks);
+}
+
+function saveTask(){
+    let tasksLength = 0;
+    let tasks = Array()
+
+    let tasksJson = localStorage.getItem("tasksJson");
+
+    //If Json tasks in local storage is not empty...
+    if (tasksJson != ""){
+        //Parses json tasks to array
+        tasks = JSON.parse(tasksJson)
+        //Redefines the length of the tasks
+        tasksLength = tasks.length;
+    }
+
+    //Picks up all the values of the form
+    let id = tasksLength + 1;
+    let title = document.getElementById('taskForm').value;
+    let explication = document.getElementById('explicationForm').value;
+    let dateToFinish = document.getElementById('dateToFinishForm').value;
+    let timeToFinish = document.getElementById('timeToFinishForm').value;
 
     //Joins up the date to finish with the time to finish through template string
-    let fullDateToFinish = dateToFinish.value + '/' + timeToFinish.value; // layout: 'YYYY-MM-DD/hh:mm'
+    let fullDateToFinish = dateToFinish + ' ' + timeToFinish; // layout: 'YYYY-MM-DD/hh:mm'
 
     //Picks up today's date
     let dateObj = new Date();
@@ -95,18 +152,19 @@ function transformToJson(){
     //formats time that the task wad added
     let dateAdded = yearAdded + '-' + monthAdded + '-' + dayAdded //layout: 'YYYY-MM-DD'
     let timeAdded = hourAdded + ':' + minAdded; //layout: 'hh:mm'
-    let fullDateAdded = dateAdded + '/' + timeAdded;
+    let fullDateAdded = dateAdded + ' ' + timeAdded;
 
     //Instantiates a task
-    let taskObj = new Task(id, task, explication, fullDateToFinish, fullDateAdded);
+    let taskObj = new Task(id, title, explication, fullDateToFinish, fullDateAdded);
     //Transform task object to JSON and save it in local storage.
     //The save is based on the id of the task
     tasks.push(taskObj);
 
     // localStorage.setItem("tasks", JSON.stringify(task));
     // console.log(localStorage.getItem("tasks"));
-    let a =JSON.stringify(tasks);
-    console.log(a);
+    
+    localStorage.setItem("tasksJson", JSON.stringify(tasks));
+
 }
     
     
